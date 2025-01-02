89cf836cd4d31d4e70128501e71ac64be660a6af
GGWPx <shandy.sandoro.55.ss@gmail.com>
2025-01-03 01:34:53 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GGWPx'
+description 'Sit Anyware'
+version '2.0'
+
+client_script 'config.lua'
+client_script 'client/*.lua'
+server_script 'server/*.lua'
+
+lua54 'yes'
